06d8323f324a6f8935db340f0db2e0518c1e6c76
Pranatheesh-S <pranatheesh@gmail.com>
2025-06-12 22:03:39 +0530

    half wrk completed in backend

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express=require('express');
 const bcrypt=require('bcrypt');
 const user=require("./models/users");
 const app=express();
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, 
+    auth: {
+        user: 'pranatheesh@gmail.com',
+        pass: 'oobykufzdpixolen', 
+    },
+});
 const cors=require('cors');
 
 app.use(express.json());
@@ -20,6 +30,20 @@ app.post('/login',async (req,res)=>{
     }
     //res.render("home");
     return res.json({message:"successful"});
+});
+
+app.post("/sending_email",(req,res)=>{
+    const {email}=req.body;
+    const code = Math.floor(1000 + Math.random() * 9000);
+    const msg = `Your Code for the Verification is: <b>${code}</b>`;
+    transporter.sendMail({
+        from: 'pranatheesh@gmail.com',
+        to: email,
+        subject: 'QuizManager verification code',
+        html: msg,
+        });
+    console.log("sent code");
+    return res.json({message : code});
 });
 
 app.listen(3000,()=>
